f3a1a3b556e8911e42d64cb84263e7ce71881ee4
Art <a.molcanovas@gmail.com>
2016-12-09 12:31:54 +0000

    travis integration test

--- src/main/java/org/alorel/netmonitor/Main.java
+++ src/main/java/org/alorel/netmonitor/Main.java
@@ -1,11 +1,21 @@
+/*
+ * Copyright (c) 2016 Arturas Molcanovas.
+ *
+ * This software is licenced under the MIT license. Please see bundled license file for more information.
+ */
+
 package org.alorel.netmonitor;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 /**
  * Created by Art on 09/12/2016.
  */
 public class Main {
-    
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> eni = NetworkInterface.getNetworkInterfaces();
     }
 }
---
+++ src/test/java/org/alorel/netmonitor/TmpTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016 Arturas Molcanovas.
+ *
+ * This software is licenced under the MIT license. Please see bundled license file for more information.
+ */
+
+package org.alorel.netmonitor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Art on 09/12/2016.
+ */
+public class TmpTest {
+
+    @Test
+    public void tmpTest() {
+        Assert.assertEquals(1, 1);
+    }
+}
